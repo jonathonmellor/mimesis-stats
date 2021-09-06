46867636013d7379f2fc7bc29a09ee37184a79ef
Jonathon Mellor <mellorjonathon1@gmail.com>
2021-09-06 11:27:05 +0100

    Added generic_distribution test

--- tests/providers/test_distribution.py
+++ tests/providers/test_distribution.py
@@ -7,14 +7,35 @@ from mimesis_stats.providers.distribution import Distribution
     "population, weights, return_value",
     [
         (["A", "B"], [0, 1], "B"),
-        (["A", "B"], [1, 0], "A"),
-        ([1, 2, 3], [0, 1, 0], 2),
         ([1, 2, 3], [1, 0, 0], 1),
     ],
 )
 def test_discrete_distribution_fixed(population, weights, return_value):
-    """Tests that do not require seed setting for deterministic results"""
+    """Test does not require seed setting for deterministic results"""
 
     generator = Distribution()
 
     assert return_value == generator.discrete_distribution(population, weights)
+
+
+@pytest.fixture
+def return_max_function(population):
+    def _(population):
+        return max(population)
+
+    return _
+
+
+@pytest.mark.parametrize(
+    "population, return_value",
+    [
+        ([1, 2, 3], 3),
+        (["a", "b", "c"], "c"),
+    ],
+)
+def test_generic_distribution_fixed(population, return_value, return_max_function):
+    """Test does not require seed setting for deterministic results"""
+
+    generator = Distribution()
+
+    assert return_value == generator.generic_distribution(func=return_max_function, population=population)
