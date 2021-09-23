699024f886e67feafbc91b1d84761ee8cfc2178c
Jonathon Mellor <mellorjonathon1@gmail.com>
2021-09-23 22:51:52 +0100

    bump package version

--- setup.py
+++ setup.py
@@ -15,13 +15,13 @@ dev_requires = read_requirements("requirements.dev.txt") + requires
 
 setuptools.setup(
     name="mimesis_stats",
-    version="0.0.1",
+    version="0.0.2",
     author="Jonathon Mellor",
     author_email="mellorjonathon1@gmail.com",
     description=("Extension of mimesis library for data generation with statistical properties"),
     long_description=long_description,
     long_description_content_type="text/markdown",
-    download_url="https://github.com/jonathonmellor/mimesis-stats/archive/refs/tags/v0.0.1.tar.gz",
+    download_url="https://github.com/jonathonmellor/mimesis-stats/archive/refs/tags/v0.0.2.tar.gz",
     url="https://github.com/jonathonmellor/mimesis-stats",
     project_urls={
         "Bug Tracker": "https://github.com/jonathonmellor/mimesis-stats/issues",
